e7b23d58fcfd8b9f4c1426ac4e31e7157cc1e02d
Pratham <jadhavprathmesh284@gmail.com>
2026-02-05 10:31:11 +0530

    bugfix(main.py): fixed cors issue

--- app/main.py
+++ app/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 from app.routes import auth_routes,task_routes
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Task Management System")
+
+# CORS CONFIG
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",  # Vite
+        "http://127.0.0.1:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_routes.router)
 app.include_router(task_routes.router)
