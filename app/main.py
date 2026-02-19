d93575742ddc495727087fd356ea909547a05aec
Pratham <jadhavprathmesh284@gmail.com>
2026-02-19 10:05:13 +0530

    Add CORS

--- app/main.py
+++ app/main.py
@@ -27,8 +27,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",   # Vite development server
-        "http://127.0.0.1:5173"
+        "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],          # Allow all HTTP methods
